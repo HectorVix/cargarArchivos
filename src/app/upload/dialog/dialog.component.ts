d6f1ff6b476805ebbc835c7aa2cddae5ac31cded
Hector <vix.hector@gmail.com>
2018-10-16 11:17:35 -0600

    cargar archivos exitoso

--- src/app/upload/dialog/dialog.component.ts
+++ src/app/upload/dialog/dialog.component.ts
@@ -41,37 +41,7 @@ export class DialogComponent implements OnInit {
     this.uploading = true;
 
     // start the upload and save the progress map
-    this.progress = this.uploadService.upload(this.files);
+   this.uploadService.upload(this.files);
 
-    // convert the progress map into an array
-    let allProgressObservables = [];
-    for (let key in this.progress) {
-      allProgressObservables.push(this.progress[key].progress);
-    }
-
-    // Adjust the state variables
-
-    // The OK-button should have the text "Finish" now
-    this.primaryButtonText = 'Finish';
-
-    // The dialog should not be closed while uploading
-    this.canBeClosed = false;
-    this.dialogRef.disableClose = true;
-
-    // Hide the cancel-button
-    this.showCancelButton = false;
-
-    // When all progress-observables are completed...
-    forkJoin(allProgressObservables).subscribe(end => {
-      // ... the dialog can be closed again...
-      this.canBeClosed = true;
-      this.dialogRef.disableClose = false;
-
-      // ... the upload was successful...
-      this.uploadSuccessful = true;
-
-      // ... and the component is no longer uploading
-      this.uploading = false;
-    });
   }
 }
--- src/app/upload/upload.service.ts
+++ src/app/upload/upload.service.ts
@@ -17,36 +17,12 @@ export class UploadService {
       const formData: FormData = new FormData();
       formData.append('file', file, file.name);
 
-      // create a http-post request and pass the form
-      // tell it to report the upload progress
-      const req = new HttpRequest('POST', url, formData, {
-        reportProgress: true
-      });
+       console.log('Archivo',file.name);
 
-      // create a new progress-subject for every file
-      const progress = new Subject<number>();
+     
+  
+      
 
-      // send the http-request and subscribe for progress-updates
-      this.http.request(req).subscribe(event => {
-        if (event.type === HttpEventType.UploadProgress) {
-
-          // calculate the progress percentage
-          const percentDone = Math.round(100 * event.loaded / event.total);
-
-          // pass the percentage into the progress-stream
-          progress.next(percentDone);
-        } else if (event instanceof HttpResponse) {
-
-          // Close the progress-stream if we get an answer form the API
-          // The upload is complete
-          progress.complete();
-        }
-      });
-
-      // Save every progress-observable in a map of all observables
-      status[file.name] = {
-        progress: progress.asObservable()
-      };
     });
 
     // return the map of progress.observables
